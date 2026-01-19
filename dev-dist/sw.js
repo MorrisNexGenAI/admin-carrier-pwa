9d56dd54d30e1c58a906bd386234caad91dfee6e
Morris <soulvibe.com@gmail.com>
2026-01-19 11:41:11 +0000

    add the fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-be266a9d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oag367ghnt8"
+    "revision": "0.jat0dp4614c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
